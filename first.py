b4f9df30e310bffa07e7afcb6cf7a2eff0cef52e
Ahsan Saeed <ranaahsan5285@gmail.com>
2023-10-23 00:38:26 -0700

    mypy

--- first.py
+++ first.py
@@ -2,3 +2,12 @@ print("ahsan");
 print("saeed");
 print("Lahore");
 print("Pakisan");
+ 
+
+name: str = "Ahsan Saeed"
+rollnum: tuple[int, str, bool] = [12, "ahsan", False]
+cond: bool = False
+count: int = 12
+floatnum: float = 1.01
+listnum: list[int] = [1, 2, 3, 4, 5]
+print(name)
